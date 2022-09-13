6b656e484074ef5ad7fbb86b162c9109d3eb580b
nostrss <jintagi@gmail.com>
2022-09-13 22:58:05 +0900

    usetitle

---
+++ usetitle.js
@@ -0,0 +1,10 @@
+const useTitle = (initailTitle) => {
+  const [title, setTitle] = useState(initailTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector('title');
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+
+  return setTitle;
+};
